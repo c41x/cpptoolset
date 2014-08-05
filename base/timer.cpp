136fd496998cb4e7c19a3935e586f94b0cf2c81f
Jakub Duracz <jakubduracz@gmail.com>
2014-08-05 10:02:38 +0200

    formatting

--- base/timer.cpp
+++ base/timer.cpp
@@ -2,54 +2,54 @@
 #include "string.h"
 #include "log.h"
 
-namespace granite{namespace base{
+namespace granite { namespace base {
 
-double timer::m_secsPerTick=1.0;
+double timer::m_secsPerTick = 1.0;
 
 bool timer::init(){
 #ifdef GE_PLATFORM_WINDOWS
 	LARGE_INTEGER freq;
-	if(QueryPerformanceFrequency(&freq)){
-		m_secsPerTick=1.0/double(freq.QuadPart);
-			//int64 freqi64=int64(freq.QuadPart);
-			//logInfo(strs("timer initialized, frequency: ",freqi64,"Hz"));
+	if(QueryPerformanceFrequency(&freq)) {
+		m_secsPerTick = 1.0 / double(freq.QuadPart);
+		//int64 freqi64 = int64(freq.QuadPart);
+		//logInfo(strs("timer initialized, frequency: ", freqi64, "Hz"));
 		return true;
 	}
-	else{
-		m_secsPerTick=1.0;
+	else {
+		m_secsPerTick = 1.0;
 		logError("timer initialization failed, could not enable high resolution timer");
-			// TODO: windows errors codes
-			// TODO: add frequency to log
-			// TODO: strings utilities
+		// TODO: windows errors codes
+		// TODO: add frequency to log
+		// TODO: strings utilities
 		return false;
 	}
 #elif defined(GE_PLATFORM_LINUX)
 	timespec ts;
-	clock_getres(CLOCK_REALTIME,&ts);
-	int64 freq=int64(ts.tv_sec)*int64(1000000000)+int64(ts.tv_nsec);
-	m_secsPerTick=1.0/double(freq);
-	logInfo(strs("timer initialized, resolution: ",freq,"ns"));
+	clock_getres(CLOCK_REALTIME, &ts);
+	int64 freq = int64(ts.tv_sec) * int64(1000000000) + int64(ts.tv_nsec);
+	m_secsPerTick = 1.0 / double(freq);
+	logInfo(strs("timer initialized, resolution: ", freq, "ns"));
 	return true;
 #else
-	m_secsPerTick=0.000001;
+	m_secsPerTick = 0.000001;
 	return true;
 #endif
 }
 
-int64 timer::tick(){
+int64 timer::tick() {
 #ifdef GE_PLATFORM_WINDOWS
 	LARGE_INTEGER li;
-	gassert(QueryPerformanceCounter(&li),"query time failed, unable to use QueryPerformanceCounter");
+	gassert(QueryPerformanceCounter(&li), "query time failed, unable to use QueryPerformanceCounter");
 	QueryPerformanceCounter(&li);
 	return li.QuadPart;
 #elif defined(GE_PLATFORM_LINUX)
 	timespec ts;
-	clock_gettime(CLOCK_REALTIME,&ts);
-	return int64(ts.tv_sec)*int64(1000000000)+int64(ts.tv_nsec);
+	clock_gettime(CLOCK_REALTIME, &ts);
+	return int64(ts.tv_sec) * int64(1000000000) + int64(ts.tv_nsec);
 #else
 	timeval tv;
-	gettimeofday(&tv,NULL);
-	return int64(tv.tv_sec)*int64(1000000)+int64(tv.tv_usec);
+	gettimeofday(&tv, NULL);
+	return int64(tv.tv_sec) * int64(1000000) + int64(tv.tv_usec);
 #endif
 }
 
--- base/timer.h
+++ base/timer.h
@@ -1,10 +1,10 @@
 /*
- * granite engine 1.0 | 2006-2013 | Jakub Duracz | jakubduracz@gmail.com | http://jakubduracz.com 
+ * granite engine 1.0 | 2006-2013 | Jakub Duracz | jakubduracz@gmail.com | http://jakubduracz.com
  * file: timer.*
  * created: 19-01-2013
- * 
+ *
  * description: high resolution timers (1ns resolution)
- * 
+ *
  * changelog:
  * - 23-09-2006: original timer in old engine
  * - 19-01-2013: file created
@@ -18,55 +18,55 @@
 
 #include "includes.h"
 
-namespace granite{namespace base{
+namespace granite { namespace base {
 
-class timer{
-		int64 m_start;
-		static double m_secsPerTick;
-		
-	public:
-		timer(){m_start=tick();}
-		~timer(){}
+class timer {
+	int64 m_start;
+	static double m_secsPerTick;
 
-		// statics
-		static bool init();
-		static int64 tick();
-		static int64 delta(const int64 &t1,const int64 &t2){return t2-t1;}
-		static inline double deltaS(const int64 &t1,const int64 &t2);
-		static inline double deltaMs(const int64 &t1,const int64 &t2);
-		static inline double deltaUs(const int64 &t1,const int64 &t2);
-		static inline double deltaNs(const int64 &t1,const int64 &t2);
+public:
+	timer() { m_start = tick(); }
+	~timer(){}
 
-		// get/set start time
-		void reset(){m_start=tick();}
-		void setStartTick(const int64 &iTick){m_start=iTick;}
-		int64 getStartTick()const{return m_start;}
+	// statics
+	static bool init();
+	static int64 tick();
+	static int64 delta(const int64 &t1,const int64 &t2) { return t2-t1; }
+	static inline double deltaS(const int64 &t1, const int64 &t2);
+	static inline double deltaMs(const int64 &t1, const int64 &t2);
+	static inline double deltaUs(const int64 &t1, const int64 &t2);
+	static inline double deltaNs(const int64 &t1, const int64 &t2);
 
-		// reset start time and get result time
-		int64 time(){int64 t=tick(), ret=delta(m_start,t); m_start=t; return ret;}
-		double timeS(){int64 t=tick(); double ret=deltaS(m_start,t); m_start=t; return ret;}
-		double timeMs(){int64 t=tick(); double ret=deltaMs(m_start,t); m_start=t; return ret;}
-		double timeUs(){int64 t=tick(); double ret=deltaUs(m_start,t); m_start=t; return ret;}
-		double timeNs(){int64 t=tick(); double ret=deltaNs(m_start,t); m_start=t; return ret;}
+	// get/set start time
+	void reset() { m_start = tick(); }
+	void setStartTick(const int64 &iTick) { m_start = iTick; }
+	int64 getStartTick()const { return m_start; }
 
-		// just give me time without changing start time
-		int64 getTime()const{return delta(m_start,tick());}
-		double getTimeS()const{return deltaS(m_start,tick());}
-		double getTimeMs()const{return deltaMs(m_start,tick());}
-		double getTimeUs()const{return deltaUs(m_start,tick());}
-		double getTimeNs()const{return deltaNs(m_start,tick());}
+	// reset start time and get result time
+	int64 time() { int64 t = tick(); ret = delta(m_start, t); m_start = t; return ret; }
+	double timeS() { int64 t = tick(); double ret = deltaS(m_start, t); m_start = t; return ret; }
+	double timeMs() { int64 t = tick(); double ret = deltaMs(m_start, t); m_start = t; return ret; }
+	double timeUs() { int64 t = tick(); double ret = deltaUs(m_start, t); m_start = t; return ret; }
+	double timeNs() { int64 t = tick(); double ret = deltaNs(m_start, t); m_start = t; return ret; }
+
+	// just give me time without changing start time
+	int64 getTime() const { return delta(m_start, tick()); }
+	double getTimeS() const { return deltaS(m_start, tick()); }
+	double getTimeMs() const { return deltaMs(m_start, tick()); }
+	double getTimeUs() const { return deltaUs(m_start, tick()); }
+	double getTimeNs() const { return deltaNs(m_start, tick()); }
 };
 
 #if defined(GE_PLATFORM_WINDOWS)
-double timer::deltaS(const int64 &t1,const int64 &t2){return double(t2-t1)*m_secsPerTick;}
-double timer::deltaMs(const int64 &t1,const int64 &t2){return double(t2-t1)*m_secsPerTick*1000.0;}
-double timer::deltaUs(const int64 &t1,const int64 &t2){return double(t2-t1)*m_secsPerTick*1000000.0;}
-double timer::deltaNs(const int64 &t1,const int64 &t2){return double(t2-t1)*m_secsPerTick*1000000000.0;}
+double timer::deltaS(const int64 &t1, const int64 &t2) { return double(t2 - t1) * m_secsPerTick; }
+double timer::deltaMs(const int64 &t1, const int64 &t2) { return double(t2 - t1) * m_secsPerTick * 1000.0; }
+double timer::deltaUs(const int64 &t1, const int64 &t2) { return double(t2 - t1) * m_secsPerTick * 1000000.0; }
+double timer::deltaNs(const int64 &t1, const int64 &t2) { return double(t2 - t1) * m_secsPerTick * 1000000000.0; }
 #else
-double timer::deltaS(const int64 &t1,const int64 &t2){return double(t2-t1);}
-double timer::deltaMs(const int64 &t1,const int64 &t2){return double(t2-t1)*0.001;}
-double timer::deltaUs(const int64 &t1,const int64 &t2){return double(t2-t1)*0.000001;}
-double timer::deltaNs(const int64 &t1,const int64 &t2){return double(t2-t1)*0.000000001;}
+double timer::deltaS(const int64 &t1, const int64 &t2) { return double(t2 - t1); }
+double timer::deltaMs(const int64 &t1, const int64 &t2) { return double(t2 - t1) * 0.001; }
+double timer::deltaUs(const int64 &t1, const int64 &t2) { return double(t2 - t1) * 0.000001; }
+double timer::deltaNs(const int64 &t1, const int64 &t2) { return double(t2 - t1) * 0.000000001; }
 #endif
 
 }}
