0814c16652342975366d5e11441d437adce59818
Jakub Duracz <jakubduracz@gmail.com>
2014-11-22 15:10:22 +0100

    + GLISP dirty prototype with memory bugs

--- base/glisp.h
+++ base/glisp.h
@@ -33,6 +33,15 @@ public:
 	cell() {}
 	cell(type_t t, const string &v) : type(t), s(v) {}
 	cell(type_t t, int v) : type(t), i(v){}
+
+	operator string() const {
+		const char *type_ts[] = {
+			"id",
+			"int",
+			"list"
+		};
+		return strf("type: % | i = % | s = %", type_ts[type], i, s);
+	}
 };
 typedef std::vector<cell> cells_t;
 
@@ -91,6 +100,21 @@ cells_t parse(const string &s) {
 	return cells;
 }
 
+// counts elements of given cell
+size_t countElements(cells_t::iterator c) {
+	if(c->type == cell::typeList) {
+		cells_t::iterator i = c + 1;
+		cells_t::iterator delim = c + c->i;
+		while(i <= delim) {
+			if(i->type == cell::typeList)
+				delim += i->i;
+			++i;
+		}
+		return std::distance(c, i);
+	}
+	return 1;
+}
+
 string toString(const cells_t &cells) {
 	string r;
 	for(const cell &c : cells) {
@@ -103,23 +127,30 @@ string toString(const cells_t &cells) {
 		else if(c.type == cell::typeIdentifier) {
 			r += c.s;
 		}
+		else {
+			r += "[unknown]";
+		}
 		r += " ";
 	}
 	return r;
 }
 
-//- dynamic scoping / stack / variable memory -
+//- 3) dynamic scoping / stack / variable memory -
 typedef std::tuple<string, size_t> var_t;
 typedef std::vector<var_t> vars_t; // name, stack position
 cells_t stack;
 vars_t variables;
 
+string printStack() {
+	return toStr(stack.size()) + " > " + toString(stack);
+}
+
 vars_t::iterator findVariable(const string &name) {
 	// searching backwards on stack will find variable in outer scopes
-	return std::find_if(variables.rbegin(), variables.rend(),
-						[name](const var_t &e) {
-							return std::get<0>(e) == name;
-						}).base();
+	return std::prev(std::find_if(variables.rbegin(), variables.rend(),
+									  [name](const var_t &e) {
+										  return std::get<0>(e) == name;
+									  }).base());
 }
 
 // returns variable
@@ -132,7 +163,11 @@ cells_t::iterator getVariable(const string &name) {
 		return stack.begin() + std::get<1>(*r);
 
 	// variable name not found
-	else return stack.end();
+	std::cout << "variable \"" << name << "\" not found" << std::endl;
+	for(auto v : variables) {
+		std::cout << "> " << std::get<0>(v) << " / " << std::get<1>(v) << std::endl;
+	}
+	return stack.end();
 }
 
 // checks if given iterator / address is valid
@@ -152,6 +187,12 @@ cells_t::iterator pushVariable(const string &name, size_t count) {
 	// resize stack and return iterator to first allocated element
 	stack.resize(stack.size() + count);
 	return stack.begin() + std::get<1>(variables.back());
+}
+
+// assign address to memory
+void pushVariable(const string &name, cells_t::iterator addr) {
+	std::cout << "push variable (" << name << ") addr: " << std::distance(stack.begin(), addr) << std::endl;
+	variables.push_back(std::make_tuple(name, std::distance(stack.begin(), addr)));
 }
 
 // reallocate variable
@@ -174,6 +215,160 @@ cells_t::iterator resizeVariable(const string &name, size_t insertPos, size_t el
 	return stack.end();
 }
 
+//- 4) consts and intrinsics -
+
+
+//- 5) eval -
+size_t addrd(cells_t::iterator i) {
+	return std::distance(stack.begin(), i);
+}
+
+
+/*
+void init() {
+	auto nil = pushVariable("nil", 1);
+	*nil = cell(cell::typeIdentifier, "nil");
+}
+*/
+
+std::stack<size_t> callStack;
+void tab() {
+	for(size_t i = 0; i < callStack.size(); ++i) {
+		std::cout << "  ";
+	}
+}
+cells_t::iterator eval(cells_t::iterator d) {
+	static bool aaa = false;
+	if(!aaa)
+		stack.reserve(100000);
+	tab();
+	std::cout << "eval " << string(*d)
+			  << "  > stack " << printStack() << std::endl;
+
+	if(d->type == cell::typeInt) {
+		stack.push_back(*d);
+		return --stack.end();
+	}
+	else if(d->type == cell::typeIdentifier) {
+		auto addr = getVariable(d->s);
+		if(isVariableValid(addr))
+			return addr;
+		// variable not found
+	}
+	else if(d->type == cell::typeList) {
+		// empty list evaluates to nil
+		if(d->i == 0) {
+			// return empty
+			std::cout << "empty list" << std::endl;
+		}
+
+		// first argument must be identifier
+		cells_t::iterator fxName = d + 1;
+		if(fxName->type != cell::typeIdentifier) {
+			// function name must be id
+			std::cout << "function name must be ID" << std::endl;
+		}
+
+		// is fx name built in function
+		if(fxName->s == "defvar") {
+			// 3 elements min!
+			cells_t::iterator varName = d + 2;
+			cells_t::iterator varValue = eval(d + 3);
+			pushVariable(varName->s, varValue);
+			return varValue;
+		}
+		else if(fxName->s == "quote") {
+			size_t elemsCount = countElements(d + 2);
+			size_t whence = stack.size();
+			stack.resize(stack.size() + elemsCount);
+			std::copy(d + 2, d + 2 + elemsCount, stack.begin() + whence);
+			return stack.begin() + whence;
+		}
+		else if(fxName->s == "lambda") {
+			size_t lambdaBodyCount = d->i - 1; // not counting name
+			size_t elemsCount = countElements(d) - 2; // not counting list and name
+			size_t whence = stack.size();
+			stack.resize(stack.size() + elemsCount + 1);
+			std::copy(d + 2, d + 2 + elemsCount, stack.begin() + whence + 1);
+			stack[whence] = cell(cell::typeList, lambdaBodyCount);
+			return stack.begin() + whence;
+		}
+		else if(fxName->s == "+") {
+			tab(); std::cout << "+" << std::endl;
+			// sum
+			size_t count = d->i - 1;
+			int res = 0;
+			for(size_t i = 0, j = 0; j < count; ++j) {
+				callStack.push(stack.size());
+				res += eval(d + 2 + i)->i;
+				stack.resize(callStack.top());
+				callStack.pop();
+
+				if((d + 2 + i)->type == cell::typeList)
+					i += countElements(d + 2 + i);
+				else ++i;
+			}
+
+			// return
+			size_t retPos = stack.size();
+			stack.resize(stack.size() + 1);
+			cell &ret = stack[retPos];
+			ret.type = cell::typeInt;
+			ret.i = res;
+
+			return stack.begin() + retPos;
+		}
+
+		// get fx address
+		cells_t::iterator fx = getVariable(fxName->s);
+		if(isVariableValid(fx)) {
+			if(fx->type == cell::typeList) {
+				int count = fx->i;
+				cells_t::iterator args = fx + 1;
+				cells_t::iterator args_vals = d + 2;
+
+				// bind args
+				// args->i == (d + 2)->i
+				for(int i = 0; i < args->i; ++i) {
+					auto v = eval(args_vals + i);
+					pushVariable((args + i + 1)->s, v);
+					std::cout << (args + i + 1)->s << " = "
+							  << (args_vals + i)->i << std::endl;
+				}
+
+				// evaluate body
+				int bodyCount = count - 1;
+				cells_t::iterator body = fx + 1 + countElements(fx + 1);
+				cells_t::iterator ret;
+				for(int i = 0, j = 0; j < bodyCount; ++j) {
+					tab();std::cout << " > body eval: " << j << std::endl;
+					tab();std::cout << " > body is: " << std::distance(stack.begin(), body + i)
+									<< std::endl;
+					if(j != count - 1)
+						;//callStack.push(stack.size());
+
+					ret = eval(body + i);
+					tab();std::cout << " >" << string(*ret) << std::endl;
+
+					if(j != count - 1) {
+						//stack.resize(callStack.top());
+						//callStack.pop();
+					}
+
+					if((body + i)->type == cell::typeList)
+						i += countElements(body + i);
+					else ++i;
+				}
+				return ret;
+			}
+			std::cout << "not function!" << std::endl;
+			return fx;
+		}
+	}
+
+	// tmp
+	return stack.end();
+}
 
 /*
 
--- tests/glisp/main.cpp
+++ tests/glisp/main.cpp
@@ -4,17 +4,23 @@ using namespace granite;
 using namespace granite::base;
 
 void repl() {
-	initInterpreter();
-	eval(parse("(defun addd (a b c) (+ a a b b c c))"));
-	while(true) {
-		std::cout << "> ";
-		string line;
-		std::getline(std::cin, line);
-		std::cout << toString(eval(parse(line))) << std::endl;
-	}
+	//eval(parse("(defun addd (a b c) (+ a a b b c c))"));
+	//eval(parse("(defvar addd (lambda (a b c) (+ a a b b c c)))"));
+
+	//std::cout << toString(parse("(defvar addd (lambda (a b c) (+ a a b b c c))) (1 2 3)")) << std::endl;
+	//std::cout << toString(parse("(defun addd (a b c) (+ a a b b c c))")) << std::endl;
+	string inp;
+	std::getline(std::cin, inp);
+	auto code = parse(inp);
+	std::cout << toString(code) << std::endl;
+	auto retAddr = eval(code.begin());
+	std::cout << "return addr: " << addrd(retAddr) << std::endl;
+	std::cout << printStack() << std::endl;
 }
 
-int main(int argc, char**argv){
-	repl();
+int main(int argc, char**argv) {
+	while(true) {
+		repl();
+	}
 	return 0;
 }
