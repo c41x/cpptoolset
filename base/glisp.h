3d9051caa7f752fdebf786d508d7075b4a9e8288
Jakub Duracz <jakubduracz@gmail.com>
2014-11-24 19:45:31 +0100

    + GLISP intrinsics, refarctor, fixes

--- base/glisp.h
+++ base/glisp.h
@@ -148,27 +148,15 @@ string printStack() {
 
 vars_t::iterator findVariable(const string &name) {
 	// searching backwards on stack will find variable in outer scopes
-	return std::prev(std::find_if(variables.rbegin(), variables.rend(),
-									  [name](const var_t &e) {
-										  return std::get<0>(e) == name;
-									  }).base());
-}
+	auto r = std::find_if(variables.rbegin(), variables.rend(),
+						  [name](const var_t &e) {
+							  return std::get<0>(e) == name;
+						  });
 
-// returns variable
-cell_t getVariable(const string &name) {
-	// find variable
-	auto r = findVariable(name);
-
-	// if variable found -> return iterator
-	if(r != variables.end())
-		return stack.begin() + std::get<1>(*r);
-
-	// variable name not found
-	std::cout << "variable \"" << name << "\" not found" << std::endl;
-	for(auto v : variables) {
-		std::cout << "> " << std::get<0>(v) << " / " << std::get<1>(v) << std::endl;
-	}
-	return stack.end();
+	// return proper invalid pointer
+	if(r == variables.rend())
+		return variables.end();
+	return std::prev(r.base());
 }
 
 // checks if given iterator / address is valid
@@ -178,6 +166,23 @@ bool isVariableValid(cell_t i) {
 
 bool isVariableValid(vars_t::iterator i) {
 	return i != variables.end();
+}
+
+// returns variable
+cell_t getVariable(const string &name) {
+	// find variable
+	auto r = findVariable(name);
+
+	// if variable found -> return iterator
+	if(isVariableValid(r))
+		return stack.begin() + std::get<1>(*r);
+
+	// variable name not found
+	std::cout << "variable \"" << name << "\" not found" << std::endl;
+	for(auto v : variables) {
+		std::cout << "> " << std::get<0>(v) << " / " << std::get<1>(v) << std::endl;
+	}
+	return stack.end();
 }
 
 // push variable and allocate memory
@@ -199,10 +204,34 @@ void pushVariable(const string &name, cell_t addr) {
 // push data on top of stack (copy all addr cell) return data address on stack
 cell_t pushData(cell_t addr) {
 	size_t elemsCount = countElements(addr);
-	size_t whence = stack.size();
+	cell_t whence = stack.end();
 	stack.resize(stack.size() + elemsCount);
-	std::copy(addr, addr + elemsCount, stack.begin() + whence);
-	return stack.begin() + whence;
+	std::copy(addr, addr + elemsCount, whence);
+	return whence;
+}
+
+// push one element to stack
+cell_t pushCell(cell c) {
+	stack.push_back(c);
+	return stack.begin() + stack.size();
+}
+
+// push cdr of given list (create list without first element)
+cell_t pushCdr(cell_t l) {
+	size_t elemsCount = countElements(l);
+	size_t elemsCountFirst = countElements(l + 1);
+	size_t realElemsCount = elemsCount - elemsCountFirst;
+	cell_t whence = stack.end();
+	stack.push_back(cell(cell::typeList, l->i - 1)); // logical elements without first element
+	stack.resize(stack.size() + realElemsCount - 1);
+	std::copy(l + 1 + elemsCountFirst, l + elemsCount, whence + 1);
+	return whence;
+}
+
+// push car of given list (basically just copy cell to stack)
+cell_t pushCar(cell_t l) {
+	stack.push_back(*l);
+	return stack.begin() + stack.size();
 }
 
 // reallocate variable
@@ -242,14 +271,56 @@ void popCallStack() {
 	callStack.pop();
 }
 
-//- 4) consts and intrinsics -
-
-
-//- 5) eval -
+//- 4) initialization consts and intrinsics -
 
 // shortcuts to constants
 cell_t c_nil;
 cell_t c_t;
+
+typedef std::function<cell_t(cell_t)> intrinsic_fx_t;
+typedef std::tuple<string, intrinsic_fx_t> intrinsic_tuple_t;
+typedef std::vector<intrinsic_tuple_t> intrinsics_t;
+typedef intrinsics_t::iterator intrinsic_t;
+intrinsics_t intrinsics;
+
+cell_t c_message(cell_t c) {
+	std::cout << "*intrinsic* ";
+	std::cout << (c + 1)->i << std::endl;
+	return c;
+}
+
+cell_t c_mul(cell_t c) {
+	std::cout << "*intrinsic* ";
+	int r = 1;
+	for(cell_t i = c + 1; i != c + 1 + c->i; ++i) {
+		r *= i->i;
+	}
+	return pushCell(cell(cell::typeInt, r));
+}
+
+// search for intrinsic address
+intrinsic_t getIntrinsic(const string &name) {
+	std::cout << "get intrinsic: " << name << std::endl;
+	return std::find_if(std::begin(intrinsics), std::end(intrinsics),
+						[name](intrinsic_tuple_t &e) {
+							return name == std::get<0>(e);
+						});
+}
+
+// test intrinsic iterator for validity
+bool isIntrinsicValid(intrinsic_t i) {
+	return i != intrinsics.end();
+}
+
+// adds intrinsic to list
+bool addIntrinsic(const string &name, intrinsic_fx_t fx) {
+	if(!isIntrinsicValid(getIntrinsic(name))) {
+		intrinsics.push_back(std::make_tuple(name, fx));
+		return true;
+	}
+	else ; // intrinsic already defined
+	return false;
+}
 
 // initialize interpreter
 void init(size_t stackSize) {
@@ -263,7 +334,13 @@ void init(size_t stackSize) {
 	auto t = pushVariable("t", 1);
 	*t = cell(cell::typeIdentifier, "t");
 	c_t = t;
+
+	// intrinsics
+	addIntrinsic("*", &c_mul);
+	addIntrinsic("message", &c_message);
 }
+
+//- 5) eval -
 
 void tab() {
 	for(size_t i = 0; i < callStack.size(); ++i) {
@@ -287,13 +364,11 @@ cell_t eval(cell_t d) {
 	}
 	else if(d->type == cell::typeList) {
 		// empty list evaluates to nil
-		if(d->i == 0) {
-			// return empty
-			//std::cout << "empty list" << std::endl;
+		if(d->i == 0)
 			return c_nil;
-		}
 
 		// first argument must be identifier
+		// TODO: or lambda/list
 		cell_t fxName = d + 1;
 		if(fxName->type != cell::typeIdentifier) {
 			std::cout << "function name must be ID" << std::endl;
@@ -312,13 +387,8 @@ cell_t eval(cell_t d) {
 			return pushData(d + 2);
 		}
 		else if(fxName->s == "lambda") {
-			size_t lambdaBodyCount = d->i - 1; // not counting name
-			size_t elemsCount = countElements(d) - 2; // not counting list and name
-			size_t whence = stack.size();
-			stack.resize(stack.size() + elemsCount + 1);
-			std::copy(d + 2, d + 2 + elemsCount, stack.begin() + whence + 1);
-			stack[whence] = cell(cell::typeList, lambdaBodyCount);
-			return stack.begin() + whence;
+			// copy cdr of lambda, first element is "lambda" identifier, we dont need it
+			return pushCdr(d);
 		}
 		else if(fxName->s == "+") {
 			tab(); std::cout << "+" << std::endl;
@@ -344,6 +414,8 @@ cell_t eval(cell_t d) {
 
 			return stack.begin() + retPos;
 		}
+		// TODO: undef variable / delete variable / test variable
+		// TODO: intrinsic, simple array ops? what kind of functionality needed?
 		else if(fxName->s == "if") {
 			// test
 			pushCallStack();
@@ -411,199 +483,23 @@ cell_t eval(cell_t d) {
 			std::cout << "not function!" << std::endl;
 			return fx;
 		}
+		else {
+			intrinsic_t i = getIntrinsic(fxName->s);
+			if(isIntrinsicValid(i)) {
+				std::cout << "valid intrinsic: " << std::get<0>(*i) << std::endl;
+				cell_t r = stack.end();
+				pushCell(cell(cell::typeList, d->i - 1)); // save count
+				for(cell_t a = d + 2; a != d + 2 + d->i - 1; ++a) // COMPLEX call!
+					eval(a);
+				std::cout << "calling intrinsic" << std::endl;
+				return std::get<1>(*i)(r);
+			}
+			std::cout << "intrinsic not found" << std::endl;
+		}
 	}
 
-	// tmp
+	// tmp, should report some error (syntax, logic error?)
 	return stack.end();
 }
-
-/*
-
-// consts
-const cell true_cell(cell::typeIdentifier, "#t");
-const cell false_cell(cell::typeIdentifier, "#f");
-const cell nil(cell::typeIdentifier, "nil");
-
-//- fx
-cell fx_add(const std::vector<cell> &c) {
-	int v = 0;
-	for(auto &e : c)
-		v += fromStr<int>(e.val);
-	return cell(cell::typeInt, toStr(v));
-}
-
-cell fx_print(const std::vector<cell> &c) {
-	for(const auto &e : c) {
-		std::cout << e.val;
-	}
-	std::cout << std::endl;
-	return c.back();
-}
-
-//- interpreter core
-std::stack<scope> call_stack;
-
-void initInterpreter() {
-	stack.resize(1024);
-	variables.resize(1024);
-	stack_top = -1;
-
-	call_stack.push(scope());
-	scope *g = &call_stack.top();
-
-	g->push("nil", nil);
-	g->push("#f", false_cell);
-	g->push("#t", true_cell);
-	g->push("add", cell(cell::typeFunction, &fx_add));
-	g->push("+", cell(cell::typeFunction, &fx_add));
-	g->push("print", cell(cell::typeFunction, &fx_print));
-	g->push("pi", cell(cell::typeInt, "3.14"));
-}
-
-cell eval(cell c) {
-	scope *s = &call_stack.top();
-	if(c.type == cell::typeIdentifier) {
-		cell *v = s->get(c.val);
-		if(!v)  {
-			std::cout << strf("- undefined variable %.", c.val) << std::endl;
-			return nil;
-		}
-		return *v;
-	}
-	else if(c.type == cell::typeInt) {
-		return c;
-	}
-	else if(c.cdr.empty()) {
-		return nil;
-	}
-	else if(c.cdr[0].type == cell::typeIdentifier) {
-		if(c.cdr[0].val == "quote")
-			return c.cdr[1];
-		else if(c.cdr[0].val == "if") {
-			bool if_p = eval(c.cdr[1]).val != "nil";
-			bool else_p = c.cdr.size() > 3;
-			return eval(if_p ? c.cdr[2] : (else_p ? c.cdr[3] : nil));
-		}
-		else if(c.cdr[0].val == "defvar") {
-			return *s->push(c.cdr[1].val, eval(c.cdr[2]));
-		}
-		else if(c.cdr[0].val == "lambda") {
-			c.type = cell::typeLambda;
-			return c;
-		}
-		else if(c.cdr[0].val == "defun") {
-			c.type = cell::typeLambda;
-			string name = c.cdr[1].val;
-			c.cdr.erase(c.cdr.begin() + 1);
-			s->push(name, c);
-			return c;
-		}
-		else {
-			// intrinsic / lambda call
-			cell proc(eval(c.cdr[0]));
-			std::vector<cell> body;
-			for(auto e = c.cdr.begin() + 1; e != c.cdr.end(); ++e) {
-				body.push_back(eval(*e));
-			}
-
-			// call
-			if(proc.type == cell::typeLambda) {
-				call_stack.push(scope());
-				for(size_t i = 0; i < proc.cdr[1].cdr.size(); ++i) // check if size is the same
-					call_stack.top().push(proc.cdr[1].cdr[i].val, body[i]);
-				cell ret;
-				for(size_t i = 2; i < proc.cdr.size(); ++i)
-					ret = eval(proc.cdr[i]);
-				call_stack.pop();
-				return ret;
-			}
-			else if(proc.type == cell::typeFunction) {
-				return proc.fx(body);
-			}
-		}
-	}
-
-	std::cout << "- unknown cell type!" << std::endl;
-	return nil;
-}
-
-cell parse(const string &s) {
-	enum tokenType {
-		tokenSymbol = -1,
-		tokenWhiteSpace,
-		tokenOpenPar,
-		tokenClosePar,
-		tokenComment,
-		tokenString,
-		tokenOperator,
-		tokenSymbolicOperator
-	};
-
-	tokenizer tok;
-	tok.addRule(tokenWhiteSpace, true, " \t\v\n\r");
-	tok.addRule(tokenOpenPar, false, "(");
-	tok.addRule(tokenClosePar, false, ")");
-	tok.addRule(tokenComment, true, ";", "", "\n\r");
-	tok.addRule(tokenString, false, "\"", "\\", "\"");
-
-	cell r;
-	std::stack<std::vector<cell>*> cp;
-
-	//TODO: minimize output (function names?)
-	for(auto t = tok.begin(s, false); t; t = tok.next()) {
-		if(t.id == tokenOpenPar) {
-			if(cp.empty()) {
-				r.type = cell::typeList;
-				cp.push(&r.cdr);
-			}
-			else {
-				cp.top()->push_back(cell(cell::typeList));
-				cp.push(&cp.top()->back().cdr);
-			}
-		}
-		else if(t.id == tokenClosePar) {
-			cp.pop();
-		}
-		else if(t.id == tokenSymbol) {
-			cell cc;
-
-			// detect type
-			if(isInteger(t.value))
-				cc.type = cell::typeInt;
-			else cc.type = cell::typeIdentifier;
-			cc.val = t.value;
-
-			// insert/return atom
-			if(cp.empty()) {
-				return cc;
-			}
-			else {
-				cp.top()->push_back(cc);
-			}
-		}
-	}
-
-	return r;
-}
-
-string toString(const cell &c) {
-	string r;
-	if(c.type == cell::typeList) {
-		bool first = true;
-		for(auto a : c.cdr) {
-			r += (first ? "(" : ", ") + toString(a);
-			first = false;
-		}
-		return r + ")";
-	}
-	else if(c.type == cell::typeInt) {
-		return c.val;
-	}
-	else if(c.type == cell::typeFunction) {
-		return "<func>";
-	}
-	return c.val;
-}
-*/
 
 }}
