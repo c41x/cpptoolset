91781995e3a56919a0d43f2b7d7d9efe647a8a1b
Jakub Duracz <jakubduracz@gmail.com>
2014-12-03 21:14:21 +0100

    + test cases for glisp updated

--- tests/glisp/main.cpp
+++ tests/glisp/main.cpp
@@ -25,5 +25,6 @@ int main(int argc, char**argv) {
 	}
 	return 0;
 }
-//(defvar l (lambda (a bc ) (+ a bc (* 2 bc) 1000)))
-//(if t (progn (message 11) (message 22)) (message 33))
+// (defvar l (lambda (a bc ) (+ a bc (* 2 bc) 1000)))
+// (if t (progn (message 11) (message 22)) (message 33))
+// (let ((a 10) (b 20)) (+ a a) (+ b b))
