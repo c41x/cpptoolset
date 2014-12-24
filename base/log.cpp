05ec1d7512481502fe9b357937f81d89ef28a1fe
Jakub Duracz <jakubduracz@gmail.com>
2014-12-24 19:35:04 +0100

    + signal (crash) handling

--- base/log.cpp
+++ base/log.cpp
@@ -1,50 +1,74 @@
 #include "log.h"
 #include <csignal>
+#ifdef GE_PLATFORM_LINUX
+#include <execinfo.h>
+#endif
 
 namespace granite { namespace base { namespace log {
 
 namespace {
 
-	const size_t bufferSize = 64;
-	string v_buffer[bufferSize]; // zamienic na const array (zeby bylo zaalokowane na stercie)
-	size_t v_pos = 0; // next index
-	size_t v_processBegin = 0; // where to start processing
-	size_t v_processCount = 0; // numbers to items to process
-	size_t v_size = 0; // size of buffer (if elements count in buffer is smaller than bufferSize)
-	std::fstream v_file;
+const size_t bufferSize = 64;
+string v_buffer[bufferSize]; // zamienic na const array (zeby bylo zaalokowane na stercie)
+size_t v_pos = 0; // next index
+size_t v_processBegin = 0; // where to start processing
+size_t v_processCount = 0; // numbers to items to process
+size_t v_size = 0; // size of buffer (if elements count in buffer is smaller than bufferSize)
+std::fstream v_file;
+volatile std::sig_atomic_t v_signalStatus = 0;
 
-	void signalCallback(int signal) {
+void signalCallback(int signal) {
+	// check if already processing signal (is any better way?)
+	// this is last thing that program will do
+	if (v_signalStatus == 0) {
+		v_signalStatus = signal;
+
 		string desc;
-		if(signal == SIGABRT) desc = "signal(SIGABRT) abnormal termination condition, as is e.g. initiated by abort()";
-		else if(signal == SIGFPE) desc = "signal(SIGFPE) erroneous arithmetic operation such as divide by zero";
-		else if(signal == SIGILL)	desc = "signal(SIGILL) invalid program image, such as invalid instruction";
-		else if(signal == SIGINT) desc = "signal(SIGINIT) external interrupt, usually initiated by the user";
-		else if(signal == SIGSEGV) desc = "signal(SIGSEGV) invalid memory access (segmentation fault)";
-		else if(signal == SIGTERM) desc = "signal(SIGTERM) termination request, sent to the program";
+		if(v_signalStatus == SIGABRT) desc = "signal(SIGABRT) abnormal termination condition, as is e.g. initiated by abort()";
+		else if(v_signalStatus == SIGFPE) desc = "signal(SIGFPE) erroneous arithmetic operation such as divide by zero";
+		else if(v_signalStatus == SIGILL) desc = "signal(SIGILL) invalid program image, such as invalid instruction";
+		else if(v_signalStatus == SIGINT) desc = "signal(SIGINIT) external interrupt, usually initiated by the user";
+		else if(v_signalStatus == SIGSEGV) desc = "signal(SIGSEGV) invalid memory access (segmentation fault)";
+		else if(v_signalStatus == SIGTERM) desc = "signal(SIGTERM) termination request, sent to the program";
 		else desc = "signal(?) unknown signal ID";
 
-		// log handling:
+		log(logLevelCritical, desc);
+
+		// print backtrace
+		#ifdef GE_PLATFORM_LINUX
+		void *a[10];
+		size_t s = backtrace(a, 10);
+		char **p = backtrace_symbols(a, s);
+		for (size_t i = 0; i < 10; ++i)
+			v_file << ptr[0] << std::endl;
+		free(p);
+		#endif
+
+		// shutdown all
 		shutdown();
+		// exit(1);
 	}
+}
 
-	void installSignal() {
-		// maybe use sigaction?
-		std::signal(SIGABRT,signalCallback);
-		std::signal(SIGFPE,signalCallback);
-		std::signal(SIGILL,signalCallback);
-		std::signal(SIGINT,signalCallback);
-		std::signal(SIGSEGV,signalCallback);
-		std::signal(SIGTERM,signalCallback);
-	}
+void installSignal() {
+	// maybe use sigaction (on linux)?
+	// TODO: install handlers in all threads?
+	std::signal(SIGABRT, signalCallback);
+	std::signal(SIGFPE, signalCallback);
+	std::signal(SIGILL, signalCallback);
+	std::signal(SIGINT, signalCallback);
+	std::signal(SIGSEGV, signalCallback);
+	std::signal(SIGTERM, signalCallback);
+}
 
-	void uninstallSignal() {
-		std::signal(SIGABRT, SIG_DFL);
-		std::signal(SIGFPE, SIG_DFL);
-		std::signal(SIGILL, SIG_DFL);
-		std::signal(SIGINT, SIG_DFL);
-		std::signal(SIGSEGV, SIG_DFL);
-		std::signal(SIGTERM, SIG_DFL);
-	}
+void uninstallSignal() {
+	std::signal(SIGABRT, SIG_DFL);
+	std::signal(SIGFPE, SIG_DFL);
+	std::signal(SIGILL, SIG_DFL);
+	std::signal(SIGINT, SIG_DFL);
+	std::signal(SIGSEGV, SIG_DFL);
+	std::signal(SIGTERM, SIG_DFL);
+}
 
 }
 
@@ -74,6 +98,7 @@ void log(logLevel level,const string &message) {
 }
 
 void process() {
+	// process messages
 	while(v_processCount > 0) {
 		v_file << v_buffer[v_processBegin % v_size] << std::endl; // v_processCount > 0 hence v_size > 0 too
 		v_processBegin = (v_processBegin + 1) % bufferSize;
