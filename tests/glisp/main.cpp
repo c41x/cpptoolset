cb0208b1dff6460d94f02e03d40feef4d8b89f74
Jakub Duracz <jakubduracz@gmail.com>
2014-12-25 16:13:12 +0100

    + refactor glisp

--- tests/glisp/main.cpp
+++ tests/glisp/main.cpp
@@ -1,32 +1,19 @@
 #include <base/base.h>
 
-using namespace granite;
-using namespace granite::base;
-
-void repl() {
-	//eval(parse("(defun addd (a b c) (+ a a b b c c))"));
-	//eval(parse("(defvar addd (lambda (a b c) (+ a a b b c c)))"));
-
-	//std::cout << toString(parse("(defvar addd (lambda (a b c) (+ a a b b c c))) (1 2 3)")) << std::endl;
-	//std::cout << toString(parse("(defun addd (a b c) (+ a a b b c c))")) << std::endl;
-	string inp;
-	std::getline(std::cin, inp);
-	auto code = parse(inp);
-	std::cout << toString(code) << std::endl;
-	auto retAddr = eval(code.begin(), true);
-	std::cout << "return addr: " << getAddress(retAddr) << std::endl;
-	printStack();
-}
-
 int main(int argc, char**argv) {
-	init(100000);
-	pushCallStack();
-	while(true) {
-		repl();
+	granite::base::lisp gl;
+	gl.init();
+
+	while (true) {
+		string inp;
+		std::getline(std::cin, inp);
+		gl.eval(inp);
 	}
-	popCallStack();
+
+	gl.close();
 	return 0;
 }
+
 // (defvar l (lambda (a bc ) (+ a bc (* 2 bc) 1000)))
 // (if t (progn (message 11) (message 22)) (message 33))
 // (let ((a 10) (b 20)) (+ a a) (+ b b))
