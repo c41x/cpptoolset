f685b6502e7641ff5dcc668b2421bac94d6b998b
c41x <jakubduracz@gmail.com>
2015-03-06 09:55:53 -0500

    + log testfiled update

--- tests/log/main.cpp
+++ tests/log/main.cpp
@@ -3,34 +3,37 @@
 int main(int argc, char**argv){
 	using namespace granite::base;
 
-	log::init("c:\\log.txt");
+	if (!log::init("/home/kuba/log.txt"))
+		std::cout << strerror(errno) << std::endl;
 	logInfo("logger initialized");
 	logInfo("build info: " GE_BUILD_INFO);
 
 	// output something to log
-	for(int i=0;i<100;++i){std::stringstream ss;
-		ss<<"loggser ("<<i<<")";
-			//log::log(log::logLevelOK,ss.str());
+	for (int i = 0; i < 100; ++i) {
+		std::stringstream ss;
+		ss << "loggser (" << i << ")";
+		//log::log(log::logLevelOK, ss.str());
 		logOK(ss.str());
-		if(70==i||i==71||i==80)
+		if (70 == i || i == 71 || i == 80)
 			log::process();
 	}
 
 	// print log buffer
-	for(size_t i=0;i<log::getBufferSize();++i)
-		std::cout<<"\nlog entry: "<<log::getBuffer(i);
+	for(size_t i = 0; i < log::getBufferSize(); ++i)
+		std::cout << "\nlog entry: " << log::getBuffer(i);
+	std::cout << std::endl;
 	std::cout.flush();
 
 	logInfo("testing (before crash)");
-	gassert(false,"assertion failed?");
-	gassert(true,"assertion failed?");
-	
+	gassert(false, "assertion failed?");
+	gassert(true, "assertion failed?");
+
 	// just to crash app (to check signal handling)
-	int *a=nullptr;
-	*a=666;
+	int *a = nullptr;
+	*a = 666;
 
 	logInfo("logging shutting down...");
 	log::shutdown();
-	
+
 	return 0;
 }
