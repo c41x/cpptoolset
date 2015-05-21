bbb87d734d57b7676b0efe9cfde7d435741cf87c
c41x <jakubduracz@gmail.com>
2015-05-21 19:46:35 +0200

    + vfs rewrite, quick draft

--- base/fs.cpp
+++ base/fs.cpp
@@ -1,6 +1,7 @@
 #include "fs.h"
 #include "string.h"
 #include "gstdlib.h"
+#include "lz4.h"
 
 #include <regex>
 #include <sys/stat.h>
@@ -26,6 +27,10 @@ string fullPath(directoryType type, const string &p = "") {
 bool _exists(const string &path) {
 	struct stat buffer;
 	return stat(path.c_str(), &buffer) == 0;
+}
+
+void _resize(std::FILE *f, size_t newSize) {
+	_chsize(_fileno(f), newSize);
 }
 
 fileList _filterFileList(const string &basePath,
@@ -127,7 +132,12 @@ bool open(const string &path, directoryType type) {
 			_dirProgramData = path;
 		else if (type == workingDirectory)
 			_dirWorkingDir = path;
-		// TODO: search for vfs files and cache them
+
+		// scan for vfs files
+		fileList fl = matchFiles(".*\\.gfs", "", type);
+		for (const auto &f : fl) {
+
+		}
 		return true;
 	}
 	gassert(false, strs("specified path does not exists: ", path));
@@ -135,7 +145,7 @@ bool open(const string &path, directoryType type) {
 }
 
 void close() {
-	// TODO: delete cache
+	// TODO: delete cache, invalidate paths?
 }
 
 fileList listFiles(const string &path, directoryType type) {
@@ -199,4 +209,311 @@ bool exists(const string &name, directoryType type) {
 	return _exists(fullPath(type, name));
 }
 
+//- buffer pool
+/*
+const _minPool = 2;
+const _poolSize = 1024 * 1024 * 16; // 16MB
+std::vector<stream> _spool_s;
+std::vector<size_t> _spool_free;
+
+stream &_spool_get() {
+}
+
+void _spool_free(stream &s) {
+	_spool.free
+}
+*/
+//- vfs
+/* gfs file structure
+// header
+char id[4]; // "GFS2"
+uint64 headerOffset;
+
+// data
+< for each file:
+uin8 rawData[size];
+>
+
+// index
+< for each file:
+uint32 filesCount;
+uint32 nameLength;
+char name[];
+uint64 position;
+uint64 size;
+uint8 flags;
+uint64 modTime;
+uint64 createTime;
+>
+*/
+
+// file entry
+struct vfs_file {
+	string id;
+	uint64 position;
+	uint64 size;
+	uint8 flags; // 1 - compressed, 2 - directory
+	uint64 createTime;
+	uint64 modTime;
+};
+
+// index for (real) file
+struct vfs {
+	std::vector<vfs_file> files;
+	uint64 indexOffset;
+	std::FILE *f;
+	bool dirty;
+};
+
+// index map
+std::map<string, vfs> _vfs;
+
+// writes index to file (write pointer must be set before call)
+void _vfs_write_index(vfs &v) {
+	uint32 filesCount = v.files.size();
+	std::fwrite(&filesCount, sizeof(uint32), 1, v.f);
+
+	// write index to stream and write
+	stream s;
+	s.reserve(filesCount * (4 + 10 + 8 + 8 + 1 + 8 + 8)); // estimate size
+	for (const auto &f : v.files) {
+		s.write(f.id);
+		s.write(f.position);
+		s.write(f.size);
+		s.write(f.flags);
+		s.write(f.createTime);
+		s.write(f.modTime);
+	}
+	std::fwrite(s.data(), s.size(), 1, v.f);
+
+	// trim file here
+	_resize(v.f, std::ftell(v.f));
+}
+
+// (re) opens/creates vfs, initializes index
+void _vfs_open(const string path) {
+	if (!_exists(path)) {
+		// create and initialize new vfs
+		uint32 filesCount = 0;
+		auto &v = _vfs[path];
+		v.f = std::fopen(path.c_str(), "wb+");
+		v.indexOffset = 4 + sizeof(uint64);
+		v.dirty = false;
+		std::fwrite("GFS2", 4, 1, v.f);
+		std::fwrite(&v.indexOffset, sizeof(uint64), 1, v.f);
+		std::fwrite(&filesCount, sizeof(uint32), 1, v.f);
+	}
+	else {
+		auto vt = _vfs.find(path);
+		if (vt == _vfs.end()) {
+			// create index
+			auto &v = _vfs[path];
+			uint32 filesCount = 0;
+			v.f = fopen(path.c_str(), "rb+");
+			v.dirty = false;
+
+			// read and validate header
+			char id[4];
+			std::fread(id, 4, 1, v.f);
+			if (id[0] != 'G' || id[1] != 'F' || id[2] != 'S' || id[3] != '2')
+				std::cout << "NOT AN VFS ARCHIVE!" << std::endl;
+			std::fread(&v.indexOffset, sizeof(uint64), 1, v.f);
+
+			// read file index in one chunk
+			std::fseek(v.f, 0, SEEK_END);
+			size_t size = std::ftell(v.f) - v.indexOffset;
+			std::fseek(v.f, v.indexOffset, SEEK_SET);
+			stream s;
+			s.resize(size);
+			std::fread(s.data(), size, 1, v.f);
+
+			// read index
+			s.read(filesCount);
+			std::cout << filesCount << std::endl;
+			while (filesCount-- > 0) {
+				vfs_file fi;
+				s.read(fi.id);
+				s.read(fi.position);
+				s.read(fi.size);
+				s.read(fi.flags);
+				s.read(fi.createTime);
+				s.read(fi.modTime);
+				std::cout << fi.id << ", " << fi.position << ", " << fi.size << ", " <<
+					fi.flags << ", " << fi.createTime << ", " << fi.modTime << std::endl;
+				v.files.push_back(fi);
+			}
+		}
+		else {
+			// already in index - reopen if not opened
+			if (vt->second.f == nullptr)
+				vt->second.f = fopen(path.c_str(), "rb+");
+		}
+	}
+}
+
+void _vfs_close(vfs &v) {
+	if (v.dirty) {
+		std::fseek(v.f, v.indexOffset, SEEK_SET);
+		_vfs_write_index(v);
+
+		// update header
+		std::fseek(v.f, 4, SEEK_SET);
+		std::fwrite(&v.indexOffset, sizeof(uint32), 1, v.f);
+		v.dirty = false;
+	}
+	fclose(v.f);
+	v.f = nullptr;
+}
+
+std::vector<vfs_file>::iterator _vfs_find_file(vfs &v, const string &id) {
+	return std::find_if(v.files.begin(), v.files.end(),
+						[&id](vfs_file &f) {
+							return f.id == id;
+						});
+}
+
+bool _vfs_exists(vfs &v, const string &id) {
+	return v.files.end() != _vfs_find_file(v, id);
+}
+
+void display(const char *d, int s) {
+	std::cout << s << ": ";
+	for (int i = 0; i < s; ++i)
+		std::cout << d[i];//int(d[i]);
+	std::cout << std::endl;
+}
+
+void _vfs_read(vfs &v, const string &id, stream &s) {
+	auto f = _vfs_find_file(v, id);
+	gassert(f != v.files.end(), strs("vfs file: ", id, " not found"));
+	if (f != v.files.end()) {
+		std::fseek(v.f, f->position, SEEK_SET);
+
+		if (f->flags & 1) {
+			// decompress
+			int64 realSize = 0;
+			std::fread(&realSize, sizeof(int64), 1, v.f);
+			stream sc(f->size - sizeof(int64));
+			std::fread(sc.data(), sc.size(), 1, v.f);
+			size_t offset = s.size();
+			s.resize(offset + realSize);
+			LZ4_decompress_safe((const char*)sc.data(), (char*)s.data() + offset, sc.size(), realSize);
+		}
+		else {
+			// read
+			size_t offset = s.size();
+			s.resize(offset + f->size);
+			std::fread(s.data() + offset, f->size, 1, v.f);
+		}
+	}
+}
+
+void _vfs_remove(vfs &v, const string &id) {
+	auto f = _vfs_find_file(v, id);
+	if (f != v.files.end()) {
+		// erase data from file
+		stream buf(1024); // 1024 bytes buffer
+		size_t readed = 1;
+		size_t to = f->position, from = f->position + f->size;
+		uint64 fPos = f->position;
+		uint64 fSize = f->size;
+		gassert(from > to, "error removing file: file index corrupted");
+		while (readed != 0) {
+			std::fseek(v.f, from, SEEK_SET);
+			readed = std::fread(buf.data(), 1, buf.size(), v.f);
+			std::fseek(v.f, to, SEEK_SET);
+			std::fwrite(buf.data(), readed, 1, v.f);
+			to += readed;
+			from += readed;
+		}
+
+		// truncate file
+		_resize(v.f, to);
+
+		// remove from index
+		v.files.erase(f);
+
+		// update file index
+		for (auto &fi : v.files) {
+			if (fi.position > fPos)
+				fi.position -= fSize;
+		}
+
+		// update index offset
+		v.indexOffset -= fSize;
+
+		// mark index as dirty
+		v.dirty = true;
+	}
+	else {
+		// report error
+		log::log(log::logLevelError, strs("could not delete: ", id, " from vfs - file not present in index"));
+	}
+}
+
+void _vfs_add(vfs &v, const string &id, const stream &s, bool compress = true) {
+	auto f = _vfs_find_file(v, id);
+
+	// replace file (remove old file first)
+	uint64 createTime = std::time(0);
+	if (f != v.files.end()) {
+		_vfs_remove(v, id);
+		createTime = f->createTime; // preserve create time
+	}
+
+	// create file
+	std::fseek(v.f, v.indexOffset, SEEK_SET);
+	if (compress) {
+		stream sc(LZ4_compressBound(s.size()));
+		uint64 compressedSize = LZ4_compress_default((const char*)s.data(), (char*)sc.data(), s.size(), sc.size());
+		int64 realSize = s.size();
+		std::fwrite(&realSize, sizeof(int64), 1, v.f);
+		std::fwrite(sc.data(), compressedSize, 1, v.f);
+		v.files.push_back({id, v.indexOffset, compressedSize + sizeof(int64), 1, createTime, (uint64)std::time(0)});
+	}
+	else {
+		std::fwrite(s.data(), s.size(), 1, v.f);
+		v.files.push_back({id, v.indexOffset, s.size(), 0, createTime, (uint64)std::time(0)});
+	}
+	v.indexOffset = std::ftell(v.f);
+	v.dirty = true;
+}
+
+void vopen(string path) {
+	_vfs_open(path);
+}
+
+void vadd(string path, string id, const stream &s) {
+	auto v = _vfs.find(path);
+	if (v != _vfs.end())
+		_vfs_add(v->second, id, s);
+	else std::cout << "vfs not found" << std::endl;
+}
+
+void vget(string path, string id, stream &s) {
+	auto v = _vfs.find(path);
+	if (v != _vfs.end())
+		_vfs_read(v->second, id, s);
+	else std::cout << "vfs not found" << std::endl;
+}
+
+void vremove(string path, string id) {
+	auto v = _vfs.find(path);
+	if (v != _vfs.end())
+		_vfs_remove(v->second, id);
+	else std::cout << "vfs not found" << std::endl;
+}
+
+void vclose() {
+	for (auto &v : _vfs)
+		_vfs_close(v.second);
+}
+
 }}}
+
+// TODO: rewrite vfs file by replace, not erase & add
+// TODO: pools (buffer pool for decompression)
+// TODO: integrate vfs into API
+// TODO: asserts and logs
+// TODO: flushing vfs
+// TODO: extensions to compress (currently static)
