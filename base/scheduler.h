1c6187329a131579521c6a0974b5406ed362b9c2
c41x <jakubduracz@gmail.com>
2015-07-11 09:33:23 +0200

    + scheduler file create

---
+++ base/scheduler.h
@@ -0,0 +1,13 @@
+/*
+ * granite engine 1.0 | 2006-2015 | Jakub Duracz | jakubduracz@gmail.com | http://jakubduracz.com
+ * file: scheduler
+ * created: 13-07-2015
+ *
+ * description: multithread job scheduler
+ *
+ * changelog:
+ * - 17-09-2007: file created
+ * - 13-07-2015: rewrite
+ */
+
+#pragma once
