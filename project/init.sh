782c8345519659641f99ff84e332c3df7d92cfa2
c41x <jakubduracz@gmail.com>
2015-08-23 17:55:50 +0200

    + SSSE3 disabled on linux

--- project/init.sh
+++ project/init.sh
@@ -2,11 +2,11 @@ mkdir debug
 mkdir release
 
 cd debug
-cmake ../../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+cmake ../../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -ENABLE_SSSE3=OFF
 cd ..
 
 cd release
-cmake ../../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake ../../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -ENABLE_SSSE3=OFF
 cd ../../ext
 
 cd glfw
