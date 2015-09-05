8bcef1c5cb011f6ad1a7ee12a9a56eb5505dae81
c41x <jakubduracz@gmail.com>
2015-09-05 13:58:51 +0200

    + init modules update

--- project/init.bat
+++ project/init.bat
@@ -15,7 +15,7 @@ cd glfw
 mkdir build
 cd build
 echo Compiling GLFW
-cmake ../ -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=TRUE
+cmake ../ -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF
 cmake --build . --config Release
 cd ../..
 
@@ -40,6 +40,14 @@ mkdir build
 cd build
 echo Compiling libjpeg-turbo
 cmake ../ -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake --build . --config Release
+cd ../..
+
+cd assimp
+mkdir build
+cd build
+echo Compiling assimp
+cmake ../ -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DASSIMP_BUILD_SAMPLES=OFF -DASSIMP_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF
 cmake --build . --config Release
 cd ../..
 
--- project/init.sh
+++ project/init.sh
@@ -12,7 +12,7 @@ cd ../../ext
 cd glfw
 mkdir build
 cd build
-cmake ../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=TRUE
+cmake ../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF
 cmake --build . --config Release
 cd ../..
 
@@ -36,4 +36,11 @@ cd build
 sh ../configure
 make
 mv .libs libs
+cd ../..
+
+cd assimp
+mkdir build
+cd build
+cmake ../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DASSIMP_BUILD_SAMPLES=OFF -DASSIMP_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF
+cmake --build . --config Release
 cd ../..
