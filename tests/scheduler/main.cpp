200962f7fdc83480f502fa2d44d07a97d84c9e74
c41x <jakubduracz@gmail.com>
2016-01-14 13:36:29 +0100

    + bug fixes

--- tests/scheduler/main.cpp
+++ tests/scheduler/main.cpp
@@ -4,20 +4,6 @@
 using namespace granite;
 using namespace granite::base;
 
-int results[1000];
-void ta(int i) {
-	results[i] = i * 2;
-	std::this_thread::sleep_for(std::chrono::milliseconds(20));
-}
-
-void finisher() {
-	for (int i = 0; i < 1000; ++i) {
-		std::cout << results[i] << "\t";
-		if (i % 10 == 0)
-			std::cout << std::endl;
-	}
-}
-
 void countWords(const string &s, int begin, int end, int &result) {
 	result = std::count(s.begin() + begin, s.begin() + end, ' ') +
 		std::count(s.begin() + begin, s.begin() + end, '\t') +
@@ -30,29 +16,31 @@ void printResult(std::vector<int> &results) {
 }
 
 int main(int argc, char **argv) {
+	#ifdef GE_PLATFORM_WINDOWS
+	fs::open("c:/tmp");
+	string txt = toStr(fs::load("log-text.txt"));
+	#else
 	fs::open("/home/calx/tmp");
 	string txt = toStr(fs::load("log-text"));
+	#endif
+
 	std::vector<int> results;
 
 	scheduler s(1024);
 
-	size_t taskSize = txt.size() / 1000; // 1000 tasks
+	size_t taskSize = txt.size() / 1000 + 1; // <= 1000 tasks
 	results.resize(1001);
 	jobID wordCountFinisher = s.addJob(std::bind(printResult, std::ref(results)), jobPriorityDependent);
 	for (size_t i = 0, j = 0; i < txt.size(); i += taskSize) {
 		s.addJob(std::bind(countWords, std::ref(txt), i, std::min(i + taskSize, txt.size()), std::ref(results[j++])), jobPriorityHigh, wordCountFinisher);
 	}
-	// jobID taskFinisher = s.addJob(finisher, jobPriorityDependent);
-	// for (int i = 0; i < 1000; ++i) {
-	// 	s.addJob(std::bind(ta, i), jobPriorityHigh, taskFinisher);
-	// }
 
 	timer t;
 	t.init();
 	t.reset();
-	s.start(4);
+	s.start(8);
 	s.stop();
-	std::cout << "\nresult: " << toStr(t.timeS()) << std::endl;
+	std::cout << "\nresult: " << toStr(t.timeMs()) << " ms" << std::endl;
 
 	fs::close();
 	return 0;
