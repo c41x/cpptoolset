c0cce85bb51c4b292db83cc86abae4f730fdc576
c41x <jakubduracz@gmail.com>
2016-02-24 18:14:44 +0100

    + hotkey test

--- tests/hotkey/main.cpp
+++ tests/hotkey/main.cpp
@@ -1,22 +1,16 @@
 #include <system/system.hpp>
-/*#include <X11/Xlib.h>
-  #include <X11/Xutil.h>*/
 
 using namespace granite;
 using namespace granite::base;
 using namespace granite::system;
 
-//bool keyBound = false;
-
-//int x11errorHandler(Display *, XErrorEvent *) { keyBound = false; return 0; }
-
 int main(int argc, char**argv) {
 	bool run = true;
 	hotkey::init();
-	hotkey::add('B', modAlt, []() {
+	hotkey::add('K', modAlt, []() {
 			std::cout << "hotkey ALT + B pressed!!!" << std::endl;
 		});
-	hotkey::add(keyMediaPlayPause, modControl, [&run]() {
+	hotkey::add('K', modControl, [&run]() {
 			std::cout << "hotkey CTRL + B pressed!!!" << std::endl;
 			run = false;
 		});
@@ -24,56 +18,5 @@ int main(int argc, char**argv) {
 		hotkey::process();
 	}
 	hotkey::shutdown();
-
-
-/*
-	Display* dpy = XOpenDisplay(0);
-	Window root = DefaultRootWindow(dpy);
-	XEvent ev;
-
-	unsigned int modifiers = AnyModifier; // num lock, caps lock are modifiers too
-	int keycode = XKeysymToKeycode(dpy, XK_K);
-	Window grab_window = root;
-	Bool owner_events = False;
-	int pointer_mode = GrabModeAsync;
-	int keyboard_mode = GrabModeAsync;
-
-	XSetErrorHandler(x11errorHandler);
-	keyBound = true;
-	XSync(dpy, 0);
-	XGrabKey(dpy, keycode, modifiers, grab_window, owner_events, pointer_mode, keyboard_mode);
-	XSync(dpy, 0);
-
-	if (!keyBound) {
-		std::cout << "could not register hotkey" << std::endl;
-		XUngrabKey(dpy, keycode, modifiers, grab_window);
-		XCloseDisplay(dpy);
-		return 0;
-	}
-	XSelectInput(dpy, root, KeyPressMask);
-	XSetErrorHandler(NULL);
-
-	bool run = true;
-	while (run) {
-		XNextEvent(dpy, &ev);
-		auto mask = ev.xkey.state;
-
-		// ignore num lock, caps lock and scroll lock
-		mask &= ~Mod2Mask;
-		mask &= ~Mod3Mask;
-		mask &= ~LockMask;
-
-		switch(ev.type) {
-			case KeyPress: {
-				if (ev.xkey.state & ShiftMask != 0) {
-					std::cout << "Hot key pressed!" << std::endl;
-					XUngrabKey(dpy, keycode, modifiers, grab_window);
-					run = false;
-				}
-			}
-		}
-	}
-
-	XCloseDisplay(dpy);*/
 	return 0;
 }
