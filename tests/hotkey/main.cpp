50b0fd60bf88aad16654a2cccae6edb25ee84a30
c41x <jakubduracz@gmail.com>
2016-02-29 12:40:39 +0100

    + desc bug

--- tests/hotkey/main.cpp
+++ tests/hotkey/main.cpp
@@ -8,10 +8,10 @@ int main(int argc, char**argv) {
 	bool run = true;
 	hotkey::init();
 	hotkey::add(getKey("K"), getModifier("M"), []() {
-			std::cout << "hotkey ALT + B pressed!!!" << std::endl;
+			std::cout << "hotkey ALT + K pressed!!!" << std::endl;
 		});
 	hotkey::add(getKey("K"), getModifier("C"), [&run]() {
-			std::cout << "hotkey CTRL + B pressed!!!" << std::endl;
+			std::cout << "hotkey CTRL + K pressed!!!" << std::endl;
 			run = false;
 		});
 	while (run) {
