dc447158d2f500d74a7dceab8fa7d4949a0354a8
c41x <jakubduracz@gmail.com>
2016-12-27 11:10:01 +0100

    + removed take() function

--- base/stream.hpp
+++ base/stream.hpp
@@ -19,55 +19,54 @@ namespace granite { namespace base {
 
 class stream
 {
-	std::vector<uint8> _mem;
-	size_t _pos;
+    std::vector<uint8> _mem;
+    size_t _pos;
 public:
-	inline stream(size_t size = 0);
-	inline ~stream();
-	inline stream(stream &&s);
-	inline stream(const stream &s);
-	inline stream &operator=(stream &&s);
-	inline stream &operator=(const stream &s);
+    inline stream(size_t size = 0);
+    inline ~stream();
+    inline stream(stream &&s);
+    inline stream(const stream &s);
+    inline stream &operator=(stream &&s);
+    inline stream &operator=(const stream &s);
 
-	// TODO: other vector fx
+    // TODO: other vector fx
 
-	// vectors stuff
-	inline size_t size() const;
-	inline void resize(size_t cap);
-	inline void resize(size_t cap, const uint8 &val);
-	inline void reserve(size_t cap);
-	inline void clear();
+    // vectors stuff
+    inline size_t size() const;
+    inline void resize(size_t cap);
+    inline void resize(size_t cap, const uint8 &val);
+    inline void reserve(size_t cap);
+    inline void clear();
 
-	inline uint8 *data();
-	inline const uint8 *data() const;
+    inline uint8 *data();
+    inline const uint8 *data() const;
 
-	// rw pointer stuff
-	inline size_t read(void *data, size_t size);
-	inline size_t read_const(void *data, size_t size) const;
-	inline void write(const void *data, size_t size);
-	inline void setPosFromBegin(size_t bytes);
-	inline void setPosFromEnd(size_t bytes);
-	inline void setPosOffset(size_t bytes);
-	inline size_t getPos() const;
-	inline std::vector<uint8> &&take();
-	inline void expand(size_t additional_cap);
+    // rw pointer stuff
+    inline size_t read(void *data, size_t size);
+    inline size_t read_const(void *data, size_t size) const;
+    inline void write(const void *data, size_t size);
+    inline void setPosFromBegin(size_t bytes);
+    inline void setPosFromEnd(size_t bytes);
+    inline void setPosOffset(size_t bytes);
+    inline size_t getPos() const;
+    inline void expand(size_t additional_cap);
 
-	template <typename T> inline size_t read(T &out);
-	template <typename T> inline void write(const T &in);
-	template <typename T> inline size_t read(std::vector<T> &out);
-	template <typename T> inline void write(const std::vector<T> &in);
+    template <typename T> inline size_t read(T &out);
+    template <typename T> inline void write(const T &in);
+    template <typename T> inline size_t read(std::vector<T> &out);
+    template <typename T> inline void write(const std::vector<T> &in);
 };
 
 class const_stream {
-	const void *_data;
-	const size_t _size;
+    const void *_data;
+    const size_t _size;
 public:
-	inline const_stream(stream &&s);
-	inline const_stream(const stream &s);
-	inline const_stream(const void *m, const size_t s);
-	inline ~const_stream();
-	inline size_t size() const;
-	inline const uint8 *data() const;
+    inline const_stream(stream &&s);
+    inline const_stream(const stream &s);
+    inline const_stream(const void *m, const size_t s);
+    inline ~const_stream();
+    inline size_t size() const;
+    inline const uint8 *data() const;
 };
 
 #include "stream.inc.hpp"
--- base/stream.inc.hpp
+++ base/stream.inc.hpp
@@ -9,126 +9,122 @@ void stream::clear() { _pos = 0; return _mem.clear(); }
 
 //- stream
 stream::stream(size_t size) {
-	if (size > 0)
-		_mem.resize(size);
-	_pos = 0;
+    if (size > 0)
+        _mem.resize(size);
+    _pos = 0;
 }
 
 stream::~stream() { }
 
 stream::stream(stream &&s) {
-	_mem = std::move(s._mem);
-	_pos = std::move(s._pos);
-	s._pos = 0;
+    _mem = std::move(s._mem);
+    _pos = std::move(s._pos);
+    s._pos = 0;
 }
 
 stream::stream(const stream &s) {
-	_mem = s._mem;
-	_pos = s._pos;
+    _mem = s._mem;
+    _pos = s._pos;
 }
 
 stream &stream::operator=(stream &&s) {
-	_mem = std::move(s._mem);
-	_pos = std::move(s._pos);
-	s._pos = 0;
-	return *this;
+    _mem = std::move(s._mem);
+    _pos = std::move(s._pos);
+    s._pos = 0;
+    return *this;
 }
 
 stream &stream::operator=(const stream &s) {
-	_mem = s._mem;
-	_pos = s._pos;
-	return *this;
+    _mem = s._mem;
+    _pos = s._pos;
+    return *this;
 }
 
 size_t stream::read(void *data, size_t size) {
-	size_t n = std::min(_mem.size() - _pos, size);
-	memcpy(data, &_mem[_pos], n);
-	_pos += n;
-	return n;
+    size_t n = std::min(_mem.size() - _pos, size);
+    memcpy(data, &_mem[_pos], n);
+    _pos += n;
+    return n;
 }
 
 size_t stream::read_const(void *data, size_t size) const {
-	size_t n = std::min(_mem.size() - _pos, size);
-	memcpy(data, &_mem[_pos], n);
-	return n;
+    size_t n = std::min(_mem.size() - _pos, size);
+    memcpy(data, &_mem[_pos], n);
+    return n;
 }
 
 void stream::write(const void *data, size_t size) {
-	size_t need = std::max(_mem.size(), _pos + size);
-	_mem.resize(need);
-	memcpy(&_mem[_pos], data, size);
-	_pos += size;
+    size_t need = std::max(_mem.size(), _pos + size);
+    _mem.resize(need);
+    memcpy(&_mem[_pos], data, size);
+    _pos += size;
 }
 
 void stream::setPosFromBegin(size_t bytes) {
-	_pos = bytes;
+    _pos = bytes;
 }
 
 void stream::setPosFromEnd(size_t bytes) {
-	gassert(_mem.size() >= bytes, "index out of range");
-	_pos = _mem.size() - bytes;
+    gassert(_mem.size() >= bytes, "index out of range");
+    _pos = _mem.size() - bytes;
 }
 
 void stream::setPosOffset(size_t bytes) {
-	gassert(_pos + bytes < _mem.size(), "index out of range");
-	_pos += bytes;
+    gassert(_pos + bytes < _mem.size(), "index out of range");
+    _pos += bytes;
 }
 
 size_t stream::getPos() const {
-	return _pos;
-}
-
-std::vector<uint8> &&stream::take() {
-	return std::move(_mem);
+    return _pos;
 }
 
 void stream::expand(size_t additional_cap) {
-	_mem.reserve(_mem.capacity() + additional_cap);
+    _mem.reserve(_mem.capacity() + additional_cap);
 }
 
 template <typename T> size_t stream::read(T &out) {
-	return read(&out, sizeof(out));
+    return read(&out, sizeof(out));
 }
 
 template <typename T> void stream::write(const T &in) {
-	write(&in, sizeof(in));
+    write(&in, sizeof(in));
 }
 
 // specializations for writing strings (writing size, then string itself)
 template <> inline size_t stream::read(string &s) {
-	uint32 len;
-	size_t r = read<uint32>(len);
-	if (r > 0) {
-		s.resize(len);
-		return read(&s[0], len * sizeof(string::value_type)) + r;
-	}
-	return 0;
+    uint32 len;
+    size_t r = read<uint32>(len);
+    if (r > 0) {
+        s.resize(len);
+        return read(&s[0], len * sizeof(string::value_type)) + r;
+    }
+    return 0;
 }
 
 template <> inline void stream::write(const string &s) {
-	write<uint32>((uint32)s.size());
-	write(&s[0], s.size() * sizeof(string::value_type));
+    write<uint32>((uint32)s.size());
+    write(&s[0], s.size() * sizeof(string::value_type));
 }
 
 // overloads for writing vectors
 template <typename T> inline size_t stream::read(std::vector<T> &out) {
-	uint32 len;
-	size_t r = read<uint32>(len);
-	if (r > 0) {
-		out.resize(len);
-		for (uint32 i = 0; i < len; ++i) {
-			r += read(out[i]);
-		}
-		return r;
-	}
-	return 0;
+    uint32 len;
+    size_t r = read<uint32>(len);
+    if (r > 0) {
+        out.resize(len);
+        for (uint32 i = 0; i < len; ++i) {
+            r += read(out[i]);
+        }
+        return r;
+    }
+    return 0;
 }
 
 template <typename T> inline void stream::write(const std::vector<T> &in) {
-	write<uint32>((uint32)in.size());
-	for (size_t i = 0; i < in.size(); ++i) {
-		write(in[i]);
-	}
+    write<uint32>((uint32)in.size());
+    for (size_t i = 0; i < in.size(); ++i) {
+        write(in[i]);
+    }
 }
 
 //- const stream
@@ -141,23 +137,23 @@ const uint8 *const_stream::data() const { return (uint8*)_data; }
 
 //- string conversions
 template<> inline size_t estimateSize(const stream &s) {
-	return s.size();
+    return s.size();
 }
 
 template<> inline bool strIs<stream>(const stringRange &s) {
-	return true;
+    return true;
 }
 
 template<> inline stream fromStr<stream>(const stringRange &s) {
-	stream r;
-	r.write(&(*s.begin), s.count());
-	return r;
+    stream r;
+    r.write(&(*s.begin), s.count());
+    return r;
 }
 
 template<> inline stringRange toStr<stream>(const stream &st, string &os) {
-	gassert(os.size() >= st.size(), "buffer size is to small");
-	st.read_const(&os[0], st.size());
-	return stringRange(os.begin(), os.begin() + st.size());
+    gassert(os.size() >= st.size(), "buffer size is to small");
+    st.read_const(&os[0], st.size());
+    return stringRange(os.begin(), os.begin() + st.size());
 }
 
 // TODO: consider returning size in write functions
