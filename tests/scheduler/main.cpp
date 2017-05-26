e3f99f07dbb85d32600fe4e354ddb66bcecb5d87
c41x <jakubduracz@gmail.com>
2017-05-26 17:57:16 +0200

    + working prototype

--- tests/scheduler/main.cpp
+++ tests/scheduler/main.cpp
@@ -1,47 +1,197 @@
 #include <base/base.hpp>
 #include <base/scheduler.hpp>
+#include <condition_variable>
 
 using namespace granite;
 using namespace granite::base;
 
 void countWords(const string &s, int begin, int end, int &result) {
-	result = (int)std::count(s.begin() + begin, s.begin() + end, ' ') +
-		(int)std::count(s.begin() + begin, s.begin() + end, '\t') +
-		(int)std::count(s.begin() + begin, s.begin() + end, '\n');
+    result = (int)std::count(s.begin() + begin, s.begin() + end, ' ') +
+        (int)std::count(s.begin() + begin, s.begin() + end, '\t') +
+        (int)std::count(s.begin() + begin, s.begin() + end, '\n');
 }
 
 void printResult(std::vector<int> &results) {
-	int wordCount = std::accumulate(results.begin(), results.end(), 0);
-	std::cout << "result: " << wordCount << " words" << std::endl;
+    int wordCount = std::accumulate(results.begin(), results.end(), 0);
+    std::cout << "result: " << wordCount << " words" << std::endl;
+}
+
+const int MAX_T = 8;
+std::atomic<int> T[MAX_T];
+std::atomic<int> tip;
+bool run = true;
+
+struct worker;
+void runWorker(worker &context);
+
+struct worker {
+    std::thread t;
+    std::function<void()> work;
+    std::atomic<bool> workToDo;
+    std::mutex mtx;
+    std::condition_variable cv;
+    int id;
+    worker() : t(std::bind(runWorker, std::ref(*this))), workToDo(false) {}
+};
+
+void printState() {
+    for (int i = 0; i < MAX_T; ++i) {
+        std::cout << T[i] << " ";
+    }
+    std::cout << "  | " << tip << std::endl;
+}
+
+void runWorker(worker &context) {
+    while (run) {
+        if (context.workToDo) {
+            context.work(); // work must be done first - task scheduling is very lightweight
+            //std::cout << "&";
+            context.workToDo = false;
+
+            int ind = ++tip;
+            T[ind - 1] = context.id;
+
+            //T[context.id] = -1;
+
+            //printState();
+        }
+        /*else {
+            std::unique_lock<std::mutex> l(context.mtx);
+            context.cv.wait(l);
+            }*/
+    }
+}
+
+worker TT[MAX_T];
+
+void init() {
+    for (int i = 0; i < MAX_T; ++i) {
+        T[i] = i;
+        TT[i].id = i;
+    }
+
+    tip = MAX_T;
+}
+
+void scheduleNextTask();
+
+void schedule(std::function<void()> fx) {
+    if (tip > 0) {
+        int ind = --tip;
+        //for (int ind = 0; ind < MAX_T; ++ind)
+
+            if (T[ind] != -1) {
+                //std::cout << "#";
+                // thread is free -> acquire
+                //printState();
+                TT[T[ind]].work = fx;
+                TT[T[ind]].workToDo = true;
+                //TT[T[ind]].cv.notify_one();
+                T[ind] = -1;
+                // run fx on thread ind
+                return;
+            }
+    }
+
+    // run on this thread
+    //std::cout << ".";
+    //std::cout << std::this_thread::get_id() << " ";
+    fx();
+}
+
+// (2)
+std::atomic<int> jobsDone = {0};
+void countWords2(const string &s, int begin, int end, int &result) {
+    countWords(s, begin, end, result);
+    jobsDone++;
+
+    if (jobsDone >= 1000) {
+        run = false;
+    }
+
+    scheduleNextTask();
+    scheduleNextTask();
+    scheduleNextTask();
+    scheduleNextTask();
+    scheduleNextTask();
+    scheduleNextTask();
+}
+
+std::atomic<size_t> taskI = {0}, taskJ = {0};
+string *taskTxt;
+std::vector<int> results;
+size_t taskSize;
+
+void scheduleNextTask() {
+    size_t i = taskI.fetch_add(taskSize);
+    size_t j = taskJ.fetch_add(1);
+    if (i < taskTxt->size())
+        schedule(std::bind(countWords2, std::ref(*taskTxt), i, std::min(i + taskSize, taskTxt->size()), std::ref(results[j])));
 }
 
 int main(int argc, char **argv) {
-	#ifdef GE_PLATFORM_WINDOWS
-	fs::open("c:/tmp");
-	string txt = toStr(fs::load("log-text.txt"));
-	#else
-	fs::open("/home/calx/tmp");
-	string txt = toStr(fs::load("log-text"));
-	#endif
+    #ifdef GE_PLATFORM_WINDOWS
+    fs::open("c:/tmp");
+    string txt = toStr(fs::load("log-text.txt"));
+    #else
+    fs::open("/home/calx/tmp");
+    string txt = toStr(fs::load("log-text"));
+    #endif
 
-	std::vector<int> results;
+    taskTxt = &txt;
 
-	scheduler s(1024);
+    //#define GRANITE_SCHEDULER
+    #if defined(GRANITE_SCHEDULER)
 
-	size_t taskSize = txt.size() / 1000 + 1; // <= 1000 tasks
-	results.resize(1001);
-	jobID wordCountFinisher = s.addJob(std::bind(printResult, std::ref(results)), jobPriorityDependent);
-	for (size_t i = 0, j = 0; i < txt.size(); i += taskSize) {
-		s.addJob(std::bind(countWords, std::ref(txt), i, std::min(i + taskSize, txt.size()), std::ref(results[j++])), jobPriorityHigh, wordCountFinisher);
-	}
+    scheduler s(1024);
 
-	timer t;
-	t.init();
-	t.reset();
-	s.start(8);
-	s.stop();
-	std::cout << "\nresult: " << toStr(t.timeMs()) << " ms" << std::endl;
+    taskSize = txt.size() / 1000 + 1; // <= 1000 tasks
+    results.resize(1001, 0);
+    jobID wordCountFinisher = s.addJob(std::bind(printResult, std::ref(results)), jobPriorityDependent);
+    for (size_t i = 0, j = 0; i < txt.size(); i += taskSize) {
+        s.addJob(std::bind(countWords, std::ref(txt), i, std::min(i + taskSize, txt.size()), std::ref(results[j++])), jobPriorityHigh, wordCountFinisher);
+    }
 
-	fs::close();
-	return 0;
+    timer t;
+    t.init();
+    t.reset();
+    s.start(8);
+    s.stop();
+    std::cout << "\nresult: " << toStr(t.timeMs()) << " ms" << std::endl;
+
+
+    #else
+
+    init();
+    taskSize = txt.size() / 1000 + 1; // <= 1000 tasks
+    results.resize(1001, 0);
+
+    timer t;
+    t.init();
+    t.reset();
+    // for (size_t i = 0, j = 0; i < txt.size(); i += taskSize) {
+    //     schedule(std::bind(countWords2, std::ref(txt), i, std::min(i + taskSize, txt.size()), std::ref(results[j++])));
+    // }
+    scheduleNextTask();
+
+    while (run);
+
+    for (auto &t : TT) {
+        t.t.join();
+    }
+
+    printResult(results);
+    std::cout << "\nresult: " << toStr(t.timeMs()) << " ms" << std::endl;
+
+    #endif
+
+    fs::close();
+
+
+    int result = 0;
+    t.reset();
+    countWords(txt, 0, txt.size(), result);
+    std::cout << "should be: " << result << " in " << t.timeMs() << " ms" << std::endl;
+
+    return 0;
 }
