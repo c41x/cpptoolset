ecaf5a60ae7c7af19a52a5736bfc1632cf2c78c5
c41x <jakubduracz@gmail.com>
2018-05-06 16:26:56 +0200

    + SSSE3 enabled by default on linux
    + disabled assimp, as it is not used for now

--- project/init.sh
+++ project/init.sh
@@ -2,11 +2,11 @@ mkdir debug
 mkdir release
 
 cd debug
-cmake ../../ -G"Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DENABLE_SSSE3=OFF
+cmake ../../ -G"Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DENABLE_SSSE3=ON
 cd ..
 
 cd release
-cmake ../../ -G"Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_SSSE3=OFF
+cmake ../../ -G"Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_SSSE3=ON
 cd ../../ext
 
 cd glfw
@@ -38,9 +38,9 @@ make
 mv .libs libs
 cd ../..
 
-cd assimp
-mkdir build
-cd build
-cmake ../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DASSIMP_BUILD_SAMPLES=OFF -DASSIMP_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF
-cmake --build . --config Release
-cd ../..
+# cd assimp
+# mkdir build
+# cd build
+# cmake ../ -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DASSIMP_BUILD_SAMPLES=OFF -DASSIMP_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF
+# cmake --build . --config Release
+# cd ../..
